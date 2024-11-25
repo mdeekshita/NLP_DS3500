c5c9bc4f9ba7a32ae274ff9daffa7e979539b8cf
deekshita Madhalam <madhalam.d@northeastern.edu>
2024-11-25 13:34:34 -0500

    Update textastic_app.py

--- textastic_app.py
+++ textastic_app.py
@@ -8,10 +8,17 @@ def main():
 
     tt = Textastic()
     tt.load_text('bbt_script.txt', 'bbt')
+    tt.load_text('brooklyn99.txt', 'brooklyn99')
+    tt.load_text('friends.txt', 'friends')
     tt.load_text('himym_script.txt', 'himym')
     tt.load_text('modern_family_script', 'modern family')
     tt.load_text('new_girl_script.txt', 'new girl')
-    tt.load_text('brooklyn-99.csv', 'Brooklyn-99', parser=tp.csv_parser)
+    
+    tt.load_text('office.txt', 'office')
+    tt.load_text('parks_and_rec.txt', 'parks and rec')
+    tt.load_text('seinfeld_script.txt', 'seinfeld')
+    tt.load_text('the_good_place_script.txt', 'the good place')
+
 
     pp.pprint(tt.data)
 
