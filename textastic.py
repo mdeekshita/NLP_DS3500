ce8e07b7beb1cae7e7363ffa357da672eaac021a
deekshita Madhalam <madhalam.d@northeastern.edu>
2024-11-25 13:18:59 -0500

    Update textastic.py

--- textastic.py
+++ textastic.py
@@ -50,20 +50,16 @@ class Textastic:
         datakey --> (filelabel --> datavalue)
         """
         self.data = defaultdict(dict)
+<<<<<<< HEAD
 
-    def default_parser(self, filename):
-        """ Parse a standard text file and produce
-        extract data results in the form of a dictionary. """
-
-        results = {
-            'wordcount': Counter("To be or not to be".split(" ")),
-            'numwords' : rnd.randrange(10, 1000)
-        }
-
-        return results
     
     #once we have files in this delete the above default_parser and use this one so that it actually reads through txt files: 
-    def defaultt_parser(seld, filename):
+    def default_parser(seld, filename):
+=======
+    
+    #once we have files in this delete the above default_parser and use this one so that it actually reads through txt files: 
+    def default_parser(self, filename):
+>>>>>>> 48f3b128751e6247191c5524f495a6224f960239
 
 
         with open(filename, mode='r') as file:
